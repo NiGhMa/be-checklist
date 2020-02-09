0131a8210d2520206c779870a8b643ab46ca9f73
nighma <nighma@gmail.com>
2020-02-09 11:56:10 -0500

    Setup monorepo add ms-checklist

---
+++ ms/ms-checklist/src/main/java/org/nighma/checklist/ms/checklist/ChecklistApplication.java
@@ -0,0 +1,13 @@
+package org.nighma.checklist.ms.checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChecklistApplication.class, args);
+    }
+
+}
